de5ae7df647fec4afdc39837e45351ff0be4bedf
AI-Tony <tonyknightley87@gmail.com>
2022-12-15 13:39:18 +0000

    Complete redesign of Parser class

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,177 +1,187 @@
 #include "Parser.h"
+#include "MTM.h"
 #include <cmath>
+#include <cstdio>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 Parser::Parser()
 {
-    try {
-        parseMode();
-        parseDiscretisation();
-        parseMaterials();
-        parseThicknesses();
-        parseSpectralWindow();
-        parseAngularWindow();
-        setSignals();
-        setMTMs();
-    } catch (char const* msg) {
-        cerr << msg << endl;
-    }
+    readConfigurationFile();
+    readDesignsFiles();
 }
 
-void Parser::parseMode() {
-    file.open(filename);
-    while (getline(file, line)) {
+void Parser::readConfigurationFile() {
+    ifstream file;
+    file.open(CONF_FILE);
+    while ( getline(file, line) ) {
         istringstream lineStream(line);
         lineStream >> key;
-        if (key=="RTM") lineStream >> mode[RTM]; 
-        else if (key=="RTE") lineStream >> mode[RTE];
-        else if (key=="TTM") lineStream >> mode[TTM];
-        else if (key=="TTE") lineStream >> mode[TTE]; 
+        if (key=="RTM") lineStream >> boolalpha >> modes[RTM]; 
+        else if (key=="RTE") lineStream >> boolalpha >> modes[RTE];
+        else if (key=="TTM") lineStream >> boolalpha >> modes[TTM];
+        else if (key=="TTE") lineStream >> boolalpha >> modes[TTE]; 
+        else if (key=="discretisation") { lineStream >> value; discretisation = stoi(value); } 
+        else if (key=="spectrum_lower_limit") {lineStream >> value; spectrum_lower_limit = stoi(value); }
+        else if (key=="spectrum_upper_limit") {lineStream >> value; spectrum_upper_limit = stoi(value); }
+        else if (key=="angle_lower_limit") {lineStream >> value; angle_lower_limit = stoi(value); }
+        else if (key=="angle_upper_limit") {lineStream >> value; angle_upper_limit = stoi(value); }
+        else if (key=="incident_medium") {lineStream >> value; incident_medium = MTM::convert(value); }
+        else if (key=="transmitted_medium") {lineStream >> value; transmited_medium = MTM::convert(value); }
     }
     file.close();
+    updateSignals();
 }
 
-void Parser::parseDiscretisation()
-{
-    found = false;
-    file.open(filename);
-    while(getline(file, line))
-    {
+void Parser::writeConfigurationFile() {
+    ifstream read_file;
+    ofstream write_file;
+    vector<string> lines;
+    read_file.open(CONF_FILE);
+    while( getline(read_file, line) ) { lines.emplace_back(line); }
+    read_file.close();
+    write_file.open(CONF_FILE);
+    write_file << boolalpha;
+    for (auto &line : lines) {
         istringstream lineStream(line);
         lineStream >> key;
-        if (key=="discretisation") 
-        {
-            lineStream >> value;
-            discretisation = stoi(value);
-            found = true;
-            break;
-        }
+        if (key=="RTM") { write_file << key << " " << modes[RTM] << endl; }
+        else if (key=="RTE") { write_file << key << " " << modes[RTE] << endl; }
+        else if (key=="TTM") { write_file << key << " " << modes[TTM] << endl; }
+        else if (key=="TTE") { write_file << key << " " << modes[TTE] << endl; }
+        else if (key=="discretisation") { write_file << key << " " << discretisation << endl; }
+        else if (key=="spectrum_lower_limit") {write_file << key << " " << spectrum_lower_limit << endl; }
+        else if (key=="spectrum_upper_limit") {write_file << key << " " << spectrum_upper_limit << endl; }
+        else if (key=="angle_lower_limit") {write_file << key << " " << angle_lower_limit << endl; }
+        else if (key=="angle_upper_limit") {write_file << key << " " << angle_upper_limit << endl; }
+        else if (key=="incident_medium") {write_file << key << " " << MTM::convert(incident_medium) << endl; }
+        else if (key=="transmitted_medium") {write_file << key << " " << MTM::convert(transmited_medium) << endl; }
+        else { write_file << line << endl; }
     }
-    file.close();
-    if (found==false){ throw "Missing required data from 'tmm.conf'!\n"; }
+    write_file.close();
 }
 
-void Parser::parseSpectralWindow()
-{
-    found = false;
-    file.open(filename);
-    while(getline(file, line))
-    {
-        string value1;
-        string value2;
-        istringstream lineStream(line);
-        lineStream >> key;
-        if (key=="spectral_window") 
-        {
-            for ( int i = 0; i < 2; i++ ) {
-                lineStream >> value1;
-                lineStream >> value2;
-                if ( stoi(value1) > stoi(value2) ) {
-                    spectral_window.push_back( stoi(value1) );
-                    spectral_window.push_back( stoi(value2) );
-                } else {
-                    spectral_window.push_back( stoi(value2) );
-                    spectral_window.push_back( stoi(value1) );
-                }
-            }
-            found = true;
-            break;
-        }
-    }
-    file.close();
-    if (found==false){ throw "Missing required data from 'tmm.conf'!\n"; }
-}
-
-void Parser::parseAngularWindow() {
-    found = false;
-    file.open(filename);
-    while(getline(file, line))
-    {
-        string value1;
-        string value2;
-        istringstream lineStream(line);
-        lineStream >> key;
-        if (key=="angular_window") 
-        {
-            for ( int i = 0; i < 2; i++ ) {
-                lineStream >> value1;
-                lineStream >> value2;
-                if ( stoi(value1) < stoi(value2) ) {
-                    angular_window.push_back( stoi(value1) );
-                    angular_window.push_back( stoi(value2) );
-                } else {
-                    angular_window.push_back( stoi(value2) );
-                    angular_window.push_back( stoi(value1) );
-                }
-            }
-            found = true;
-            break;
-        }
-    }
-    file.close();
-    if (found==false){ throw "Missing required data from 'tmm.conf'!\n"; }
-}
-
-void Parser::parseMaterials()
-{
-    found = false;
-    file.open(filename);
-    while(getline(file, line))
-    {
-        istringstream lineStream(line);
-        lineStream >> key;
-        if (key=="materials") 
-        {
-            while ( lineStream >> value ) {
-                materials.push_back( MTM::convert(value) );
-            }
-            found = true;
-            break;
-        }
-    }
-    file.close();
-    if (found==false){ throw "Missing required data from 'tmm.conf'!\n"; }
-}
-
-void Parser::parseThicknesses()
-{
-    file.open("thicknesses");
-    while(getline(file, line))
-    {
+void Parser::readDesignsFiles() {
+    ifstream material_file;
+    ifstream thickness_file;
+    material_file.open(MATERIAL_FILE);
+    thickness_file.open(THICKNESS_FILE);
+    while ( getline(material_file, line) ) {
+        vector<material> materials;
         istringstream lineStream(line);
         while ( lineStream >> value ) {
-            thicknesses.push_back( stod(value) * 1e-9 );
+            materials.emplace_back( MTM::convert(value) );
         }
-        layers.push_back(thicknesses.size());
-        thicknesses_all.push_back(thicknesses);
-        thicknesses.clear();
+        all_materials.emplace_back( materials );
     }
-    file.close();
+    while ( getline(thickness_file, line) ) {
+        vector<double> thicknesses;
+        istringstream lineStream(line);
+        while ( lineStream >> value ) {
+            thicknesses.emplace_back( stod(value) * 1e-9 );
+        }
+        all_thicknesses.emplace_back( thicknesses );
+    }
+    material_file.close();
+    thickness_file.close();
+    updateDesigns();
 }
 
-void Parser::setSignals() {
-    double step = double( spectral_window[0] - spectral_window[1] ) / discretisation ;
-    for ( int theta = angular_window[0]; theta < angular_window[1]; theta++ ) {
+void Parser::writeDesignsFiles() {
+    ofstream material_file;
+    ofstream thickness_file;
+    material_file.open(MATERIAL_FILE);
+    thickness_file.open(THICKNESS_FILE);
+    for ( auto &mtm : MTMs ) {
+        for ( auto &mat : mtm.materials ) { material_file << MTM::convert(mat) << " "; }
+        for ( auto &thickness : mtm.thicknesses ) { thickness_file << thickness * 1e9 << " "; }
+        material_file << endl;
+        thickness_file << endl;
+    }
+    material_file.close();
+    thickness_file.close();
+}
+
+void Parser::setModes(mode mode, bool boolean) {
+    modes[mode] = boolean;
+}
+
+void Parser::setDisctretisation(int num) {
+    discretisation = num;
+}
+
+void Parser::setLowerSpectralLimit(int num) {
+    spectrum_lower_limit = num;
+}
+
+void Parser::setUpperSpectralLimit(int num) {
+    spectrum_upper_limit = num;
+}
+
+void Parser::setLowerAngularLimit(int num) {
+    angle_lower_limit = num;
+}
+
+void Parser::setUpperAngularLimit(int num) {
+    angle_upper_limit = num;
+}
+
+void Parser::setIncidentMedium(material mat) {
+    incident_medium = mat;
+} 
+
+void Parser::setTransmittedMedium(material mat) {
+    transmited_medium = mat;
+}
+
+void Parser::setDesigns(vector<double> thicknesses, vector<material> materials) {
+    all_materials.emplace_back( materials );
+    all_thicknesses.emplace_back( thicknesses );
+}
+
+void Parser::deleteDesigns(int num) {
+    if ( num == 0 ) {
+        MTMs.clear();
+    } else {
+        MTMs.erase( MTMs.begin() + num - 1 );
+    }
+}
+
+void Parser::updateSignals() {
+    signals.clear();
+    double step = double( spectrum_upper_limit - spectrum_lower_limit ) / discretisation ;
+    for ( int theta = angle_lower_limit; theta < angle_upper_limit; theta++ ) {
         signal sig;
         sig.theta = theta*M_PI/180;
-        sig.lower = spectral_window[1];
-        sig.upper = spectral_window[0];
-        for (double lambda = spectral_window[0]; lambda > spectral_window[1]; lambda -= step) {
+        sig.lower = spectrum_lower_limit;
+        sig.upper = spectrum_upper_limit;
+        for (double lambda = spectrum_upper_limit; lambda > spectrum_lower_limit; lambda -= step) {
             double k0 = 2.0*M_PI / ( lambda*1e-9 );
-            sig.k0.push_back( k0 );
-            sig.kx.push_back( k0*sin(sig.theta) );
-            sig.omega.push_back( 3e8 * k0 ); 
+            sig.k0.emplace_back( k0 );
+            sig.kx.emplace_back( k0*sin(sig.theta) );
+            sig.omega.emplace_back( 3e8 * k0 ); 
         }
-        signals.push_back(sig);
+        signals.emplace_back(sig);
     }
 }
 
-void Parser::setMTMs()
-{
-    for ( int i = 0; i < thicknesses_all.size(); i++ ) { 
+void Parser::updateDesigns() {
+    MTMs.clear();
+    for (int i = 0; i < all_materials.size(); i++) {
         MTM mtm;
-        mtm.layers = layers[i];
-        mtm.materials = materials;
-        mtm.thicknesses = thicknesses_all[i];
-        MTMs.push_back(mtm);
+        mtm.incident_medium = incident_medium;
+        mtm.transmited_medium = transmited_medium;
+        mtm.materials = all_materials[i];
+        mtm.thicknesses = all_thicknesses[i];
+        mtm.layers = mtm.materials.size();
+        MTMs.emplace_back( mtm );
     }
+}
+
+void Parser::refresh() {
+    updateDesigns();
+    updateSignals();
+    writeDesignsFiles();
+    writeConfigurationFile();
 }
--- src/Parser.h
+++ src/Parser.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <cstddef>
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include <sstream>
+#include <iomanip>
+#include <memory>
 #include <map>
 #include "Signal.h"
 #include "MTM.h"
@@ -13,35 +16,75 @@ using namespace std;
 
 class Parser
 {
-  public:
-    Parser();
-    map<modes, bool> mode;
-    vector<MTM> MTMs;
-    vector<signal> signals;
+    public:
+        Parser();
+        void refresh();
+        void setModes(mode, bool);
+        void setDisctretisation(int);
+        void setLowerAngularLimit(int);
+        void setUpperAngularLimit(int);
+        void setLowerSpectralLimit(int);
+        void setUpperSpectralLimit(int);
+        void setIncidentMedium(material);
+        void setTransmittedMedium(material);
+        void setDesigns(vector<double>, vector<material>);
+        void deleteDesigns(int);
+
+        vector<MTM> getMTMs() { return MTMs; }
+        vector<signal> getSignals() { return signals; }
+        bool getModes(mode mode) { return modes[mode]; }
+        int getDisctretisation() { return discretisation; }
+        int getLowerAngularLimit() { return angle_lower_limit; }
+        string getUpperAngularLimit() { 
+            if (angle_upper_limit<=angle_lower_limit) { return "NULL"; }
+            return to_string(angle_upper_limit); 
+        }
+        int getLowerSpectralLimit() { return spectrum_lower_limit; }
+        int getUpperSpectralLimit() { return spectrum_upper_limit; }
+        material getIncidentMedium() { return incident_medium; }
+        material getTransmittedMedium() { return transmited_medium; }
   
-  private:
-    ifstream file;
-    bool found;
-    string line;
-    string key;
-    string value;
-    string filename = "tmm.conf";
+    private:
+        string key;
+        string line;
+        string value;
+        string CONF_FILE = "tmm.conf";
+        string MATERIAL_FILE = "materials";
+        string THICKNESS_FILE = "thicknesses";
+        vector<MTM> MTMs;
+        vector<signal> signals;
+        map<mode, bool> modes;
+        vector<vector<double>> all_thicknesses; 
+        vector<vector<material>> all_materials;
+        material incident_medium;
+        material transmited_medium;
+        int discretisation;
+        int spectrum_lower_limit;
+        int spectrum_upper_limit;
+        int angle_lower_limit;
+        int angle_upper_limit;
+        void writeConfigurationFile();
+        void writeDesignsFiles();
+        void readConfigurationFile();
+        void readDesignsFiles();
+        void updateSignals();
+        void updateDesigns();
 
-    int discretisation;
-    vector<int> angular_window;
-    vector<int> spectral_window;
 
-    vector<material> materials;
-    vector<int> layers;
-    vector<double> thicknesses;
-    vector<vector<double>> thicknesses_all;
-
-    void parseMode();
-    void parseDiscretisation();
-    void parseSpectralWindow();
-    void parseAngularWindow();
-    void parseMaterials();
-    void parseThicknesses();
-    void setSignals();
-    void setMTMs();
+        // void setSignals();
+        // void setMTMs();
+        // vector<int> layers;
+        // unique_ptr<vector<vector<double>>> all_thicknesses{new vector<vector<double>>};
+        // vector<int> angular_window;
+        // vector<int> spectral_window;
+        // vector<double> thicknesses;
+        // vector<material> materials;
+        // vector<vector<double>> _thicknesses_all;
+        // vector<vector<material>> _materials_all;
+        // void parseMode();
+        // void parseDiscretisation();
+        // void parseSpectralWindow();
+        // void parseAngularWindow();
+        // void parseMaterials();
+        // void parseThicknesses();
 };
